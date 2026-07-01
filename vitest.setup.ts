fb1717872b1c9c4f3c1b596921ef7c8d7d80ed5e
Alfi Samudro Mulyo <alfialfarisi@gmail.com>
2026-07-02 06:12:24 +0700

    fix: remove NODE_ENV variable from vitest setup

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -6,4 +6,3 @@ process.env.NEXTAUTH_URL = "http://localhost:3000";
 process.env.GOOGLE_CLIENT_ID = "test-google-client-id";
 process.env.GOOGLE_CLIENT_SECRET = "test-google-client-secret";
 process.env.RESEND_API_KEY = "re_test_key_vitest";
-process.env.NODE_ENV = "test";
